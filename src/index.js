7d0a7a0d11f4e816dbcdf6723bc9db9bf6b9a3e0
ejun kubuntu <ejunhok@gmail.com>
2025-04-18 15:26:52 -0300

    feat: add database connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import express from 'express'
-import cors from 'cors'
 import dotenv from 'dotenv'
 
 dotenv.config();
+
+import pool from './config/db.js';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,12 +12,18 @@ const port = process.env.PORT || 3001;
 // Middlewares
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 // Routes
 
 
 // Errors handling middleware
+
+// Testing POSTGRES connection
+app.get("/", async(req, res) => {
+    const result = await pool.query("SELECT current_database()")
+    res.send(`The database name is: ${result.rows[0].current_database}`);
+})
 
 
 // Server running
